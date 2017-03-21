d90d300ddcb76dbff1f166193f71ac1f34bc4e14
sunxu <sunxu@sunxudeMacBook-Pro.local>
2017-03-21 18:19:47 +0800

    Project init

---
+++ NetEaseMusic/NetEaseMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetEaseMusic
+//
+//  Created by sunxu on 2017/3/21.
+//  Copyright © 2017年 Sun Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
